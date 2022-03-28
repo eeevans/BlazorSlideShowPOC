964d4bb44811308a5e73c3fc1b9782f46e49394c
Edward Evans <eeevans@hotmail.com>
2022-03-28 13:40:28 -0500

    Add project files.

---
+++ BlazorSlideShow/Services/MessageService.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorSlideShow.Services;
+
+public class MessageService
+{
+    public event MessageTriggeredHandler RaiseMessageTriggered;
+
+    public delegate void MessageTriggeredHandler(object sender, MessageTriggeredEventArgs args);
+}
+
+public class MessageTriggeredEventArgs : EventArgs
+{
+    public MessageTriggeredEventArgs(string newFile)
+    {
+        NewFile = newFile;
+    }
+
+    public string NewFile { get; set; }
+}
